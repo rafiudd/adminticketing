5a23853c2edab1f035457f6ce393dfdf726cf68a
Ubuntu <ubuntu@ip-172-31-90-230.ec2.internal>
2020-01-27 15:05:25 +0000

    bugfix on server

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
     database: {
-        url: 'mongodb://localhost:27017/ticketing'
+        url: 'mongodb://rafiudd:123123123@0.0.0.0:27017/ticketing'
     },
     server: {
         host: '0.0.0.0',
-        port: '9003'
+        port: '4001'
     }
 }
  
